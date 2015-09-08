4993a595f5e643a3102891453675713b5dd5e45b
pedro <hdlguy@hotmail.com>
2015-09-07 19:45:02 -0600

    make_test_data.m works

--- matlab/make_test_data.m
+++ matlab/make_test_data.m
@@ -1,13 +1,17 @@
 clear;
+Nb = 12; % number of bits in input word
+N=2^14;  % number of samples
 
-
+cr=2*pi/N; 
+w=(-N/2:N/2-1)*cr; 
+ph=cumsum(w); 
+s=exp(j*ph);
+scale = (2^(Nb-1))-1;
+s2=round(scale*s);
 
 fid = fopen('test_data.dat','w');
-for i = 1:size(r,1)
-    for j = 1:size(r,2)
-        fprintf(fid,'%d  %d  ',real(r(i,j)), imag(r(i,j)));
-    end
-    fprintf(fid,'\n');
+for i = 1:size(s,2)
+    fprintf(fid,'%d  %d \n',real(s2(i)), imag(s2(i)));
 end
 fclose(fid);
 
